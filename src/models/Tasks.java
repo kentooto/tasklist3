71f41cf3c8a1224973586fc2674d1769bccedda0
oneok <oneok@DESKTOP-KARBV3O>
2023-07-25 16:44:34 +0900

    「getAllTasksを追加」

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -7,9 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "getAllTasks", query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC")
+})
 @Table(name = "tasks")
 public class Tasks {
     @Id
@@ -34,7 +39,6 @@ public class Tasks {
         this.id = id;
     }
 
-
     public String getContent() {
         return content;
     }
@@ -58,4 +62,5 @@ public class Tasks {
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
+
 }
